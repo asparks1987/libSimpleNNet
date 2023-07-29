705b9ee13e1c5c56fc5ae28b46c5a30249c64e26
Aryn M Sparks <84827705+asparks1987@users.noreply.github.com>
2023-07-29 17:58:16 -0500

    none

--- nnet_test.py
+++ nnet_test.py
@@ -1,7 +1,2 @@
-from nnet import silvernnet
+import libsimplennet
 import numpy as np
-from pairgenerator import PairGenerator
-
-gen = PairGenerator(5,10000)
-gen.generate_pairs()
-gen.save_to_file('nnettest.pg')
