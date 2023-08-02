e985ea657d7d0c940feb7cc1cb5e34f5de1b488b
Aryn M Sparks <84827705+asparks1987@users.noreply.github.com>
2023-08-02 13:18:26 -0500

    Commit Message: "Added GUI console output and multi-threaded server functionality.
    - Introduced OutputRedirector to redirect sys.stdout to a console in the GUI.
    - Included a ServerThread to handle client connections on a different thread.
    - Implemented run_server method that starts a server on a given port.
    - Updated the GUI to include console and run button.
    "

--- libSimpleNNet.py
+++ libSimpleNNet.py
@@ -1,128 +1,170 @@
-# Import necessary libraries
-import os
+import sys
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import Callback
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import tkinter as tk
+from tkinter import ttk, filedialog, simpledialog, scrolledtext
+import numpy as np
+import socket
+import threading
 
-def load_data(file_path: str) -> pd.DataFrame:
-    """
-    Loads data from a csv file and returns a DataFrame.
+class OutputRedirector:
+    def __init__(self, app):
+        self.app = app
 
-    Parameters:
-    file_path (str): Path to the CSV file
+    def write(self, string):
+        self.app.console_insert(string)
 
-    Returns:
-    pd.DataFrame: Loaded data
+    def flush(self):
+        pass
 
-    Raises:
-    FileNotFoundError: If the file does not exist.
-    """
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(f"File not found: {file_path}")
-    return pd.read_csv(file_path)
+class ProgressCallback(Callback):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
 
+    def on_epoch_end(self, epoch, logs=None):
+        self.app.update_progress((epoch + 1) / self.params['epochs'] * 100)
+        self.app.losses.append(logs['loss'])
+        self.app.update_loss_plot()
 
 def preprocess_data(df, target_col):
-    """
-    This function preprocesses the DataFrame by converting the time and date to timestamp and normalizing all columns.
-    It also splits the data into input features and target variable.
-    """
     if target_col not in df.columns:
         raise Exception(f"Target column not found in DataFrame: {target_col}")
-        
-    # Convert the time and date to timestamp
-    df['timestamp'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+
+    df['input'] = df['input'].apply(lambda x: [float(i) for i in x.split(',')])
+    input_df = pd.DataFrame(df['input'].to_list(), columns=[f'input_{i}' for i in range(len(df['input'][0]))])
+    df = pd.concat([input_df, df.drop('input', axis=1)], axis=1)
+
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
     df['timestamp'] = (df['timestamp'] - df['timestamp'].min()) / np.timedelta64(1, 'D')
 
-    # Split data into input features and target variable
     X = df.drop(target_col, axis=1)
     y = df[target_col]
 
-    # Normalize the data
     scaler = MinMaxScaler(feature_range=(-1, 1))
     X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
     return X, y
 
-def split_data(X, y):
-    """
-    This function splits the data into training and test sets.
-    """
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
-
 def create_model(input_shape):
-    """
-    This function creates and returns a LSTM model with the specified input shape.
-    """
     model = Sequential()
     model.add(LSTM(50, activation='relu', input_shape=input_shape))
     model.add(Dense(1))
-
-    # Compile the model
     model.compile(optimizer='adam', loss='mse')
 
     return model
 
-def train_model(model, X_train, y_train, epochs=50, validation_split=0.2):
-    """
-    This function trains the model on the training data.
-    """
+def train_model(X_train, y_train, callback=None, epochs=5, validation_split=0.5):
     X_train_values = X_train.values.reshape((X_train.shape[0], X_train.shape[1], 1))
-    history = model.fit(X_train_values, y_train, epochs=epochs, validation_split=validation_split)
+
+    model = create_model((X_train.shape[1], 1))
+
+    callbacks = [callback] if callback else []
+
+    history = model.fit(X_train_values, y_train, epochs=epochs, validation_split=validation_split, callbacks=callbacks)
+
     return model, history
 
-def evaluate_model(model, X_test, y_test):
-    """
-    This function evaluates the model on the test data and returns the loss.
-    """
-    X_test_values = X_test.values.reshape((X_test.shape[0], X_test.shape[1], 1))
-    loss = model.evaluate(X_test_values, y_test)
-    return loss
+class ServerThread(threading.Thread):
+    def __init__(self, app, port):
+        super().__init__()
+        self.app = app
+        self.port = port
 
-def predict_sample(model, X_test):
-    """
-    This function predicts the value for the first sample in the test set.
-    """
-    sample = X_test.iloc[0]  # take the first sample from the test set
-    sample_reshaped = sample.values.reshape((1, sample.shape[0], 1))
-    prediction = model.predict(sample_reshaped)
-    return prediction[0][0]
+    def run(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', self.port))
+        print(f"Server started at localhost:{self.port}\n")
+        s.listen(1)
+        conn, addr = s.accept()
+        print("Connection established with client\n")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            inputs = np.fromstring(data.decode(), sep=',')
+            inputs = inputs.reshape((1, len(inputs), 1))
+            outputs = self.app.model.predict(inputs)
+            conn.sendall(str(outputs[0][0]).encode())
+        conn.close()
+        print("Connection closed\n")
 
-def save_model(model, model_path):
-    """
-    This function saves the trained model to a file.
-    """
-    model.save(model_path)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.figure = Figure(figsize=(5, 5), dpi=100)
+        self.loss_plot = self.figure.add_subplot(111)
+        self.loss_plot.set_xlabel("Epoch")
+        self.loss_plot.set_ylabel("Loss")
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
+        self.canvas.get_tk_widget().pack()
+        self.losses = []
 
-def load_saved_model(model_path):
-    """
-    This function loads a trained model from a file.
-    """
-    model = load_model(model_path)
-    return model
+    def create_widgets(self):
+        self.load_btn = tk.Button(self, text="LOAD TRAINING DATA", command=self.load_data)
+        self.load_btn.pack(side="top")
 
-# The main code execution block
-if __name__ == "__main__":
-    file_path = 'data.csv'
-    target_col = 'target'
-    model_path = 'model.h5'
+        self.train_btn = tk.Button(self, text="TRAIN NETWORK", command=self.train_network)
+        self.train_btn.pack(side="top")
 
-    try:
-        df = load_data(file_path)
-        X, y = preprocess_data(df, target_col)
-        X_train, X_test, y_train, y_test = split_data(X, y)
-        model = create_model(input_shape=(X_train.shape[1], 1))
-        model, _ = train_model(model, X_train, y_train)
-        save_model(model, model_path)
-        model = load_saved_model(model_path)
-        loss = evaluate_model(model, X_test, y_test)
-        print('Test loss:', loss)
-        prediction = predict_sample(model, X_test)
-        print('Predicted value:', prediction)
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        self.run_btn = tk.Button(self, text="RUN", command=self.run_server)
+        self.run_btn.pack(side="top")
+
+        self.progress = ttk.Progressbar(self, length=200, mode='determinate')
+        self.progress.pack(side="top")
+
+        self.console = scrolledtext.ScrolledText(self, height=10)
+        self.console.pack(side="top")
+
+    def load_data(self):
+        self.csv_file = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+        df = pd.read_csv(self.csv_file)
+        self.X_train, self.y_train = preprocess_data(df, 'target')
+
+    def update_progress(self, value):
+        self.progress["value"] = value
+        self.update_idletasks()
+
+    def update_loss_plot(self):
+        self.loss_plot.clear()
+        self.loss_plot.plot(self.losses)
+        self.canvas.draw()
+
+    def console_insert(self, text):
+        self.console.insert(tk.END, text)
+        self.console.see(tk.END)
+
+    def train_network(self):
+        if not hasattr(self, 'X_train') or not hasattr(self, 'y_train'):
+            raise Exception("No data loaded for training. Please load a data file first.")
+
+        self.progress["value"] = 0
+        self.update_idletasks()
+
+        input_shape = (self.X_train.shape[1], 1)
+        model = create_model(input_shape)
+        callback = ProgressCallback(self)
+
+        self.model, self.history = train_model(self.X_train, self.y_train, callback=callback)
+
+        self.progress["value"] = 100
+        self.update_idletasks()
+
+    def run_server(self):
+        port = simpledialog.askinteger("Port", "Enter port number")
+        if port:
+            server_thread = ServerThread(self, port)
+            server_thread.start()
+
+root = tk.Tk()
+app = Application(master=root)
+sys.stdout = OutputRedirector(app)
+app.mainloop()
--- nnet_test.py
+++ nnet_test.py
@@ -1,2 +1,119 @@
-import libsimplennet
-import numpy as np
+import sys
+import libSimpleNNet
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtCore import pyqtSlot
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import threading
+import subprocess
+import matplotlib.pyplot as plt
+
+# To run an external script
+def run_external_script(script_path='libpairgenerator\generate.py'):
+    subprocess.call(['python', script_path])
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Setting up the main window
+        self.setWindowTitle("Neural Network GUI")
+        self.setGeometry(100, 100, 800, 600)
+
+        # Placeholder for loss history
+        self.loss_history = []
+
+        # Adding buttons
+        self.load_train_button = QPushButton("Load Training Data", self)
+        self.load_train_button.move(20, 20)
+        self.load_train_button.clicked.connect(self.load_training_data)
+
+        self.load_test_button = QPushButton("Load Test Data", self)
+        self.load_test_button.move(20, 60)
+        self.load_test_button.clicked.connect(self.load_test_data)
+
+        self.save_network_button = QPushButton("Save Network", self)
+        self.save_network_button.move(20, 100)
+        self.save_network_button.clicked.connect(self.save_network)
+
+        self.load_network_button = QPushButton("Load Network", self)
+        self.load_network_button.move(20, 140)
+        self.load_network_button.clicked.connect(self.load_network)
+
+        self.run_network_button = QPushButton("Run Network", self)
+        self.run_network_button.move(20, 180)
+        self.run_network_button.clicked.connect(self.run_network)
+
+        self.run_script_button = QPushButton("Run External Script", self)
+        self.run_script_button.move(20, 220)
+        self.run_script_button.clicked.connect(self.run_script)
+
+        self.label = QLabel(self)
+        self.label.move(20, 260)
+
+        # Adding a plot
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.plot_widget = QWidget(self)
+        self.plot_widget.setGeometry(200, 20, 570, 550)
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.plot_widget.setLayout(layout)
+
+    # Function to load training data
+    @pyqtSlot()
+    def load_training_data(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open training data file", "", "CSV Files (*.csv)")
+        # load your training data using the filename
+        self.label.setText("Training data loaded from " + filename)
+        
+    # Function to load test data
+    @pyqtSlot()
+    def load_test_data(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open test data file", "", "CSV Files (*.csv)")
+        # load your test data using the filename
+        self.label.setText("Test data loaded from " + filename)
+
+    # Function to save the network
+    @pyqtSlot()
+    def save_network(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save network", "", "H5 Files (*.h5)")
+        # save your network to the filename
+        self.label.setText("Network saved to " + filename)
+
+    # Function to load the network
+    @pyqtSlot()
+    def load_network(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open network file", "", "H5 Files (*.h5)")
+        # load your network from the filename
+        self.label.setText("Network loaded from " + filename)
+
+    # Function to run the network
+    @pyqtSlot()
+    def run_network(self):
+        # run your network here, and update self.loss_history with the loss values
+        self.label.setText("Running the network...")
+        self.update_plot()
+
+    # Function to run an external script
+    @pyqtSlot()
+    def run_script(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open Python script", "", "Python Files (*.py)")
+        self.label.setText("Running external script from " + filename)
+        threading.Thread(target=run_external_script, args=(filename,)).start()
+
+    # Function to update the plot
+    def update_plot(self):
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        ax.plot(self.loss_history)
+        self.canvas.draw()
+
+
+# Running the application
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
